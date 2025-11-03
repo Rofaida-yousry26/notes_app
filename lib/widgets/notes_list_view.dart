e274468b11f1a40f6dab763d8ca7d4678a185e7e
Rofaida-yousry26 <rofyyousry2004@gmail.com>
2025-11-03 15:52:21 +0200

    add floating action botton

--- lib/widgets/notes_list_view.dart
+++ lib/widgets/notes_list_view.dart
@@ -4,11 +4,18 @@ import 'package:notes_application/widgets/custom_note_item.dart';
 class NoteListView extends StatelessWidget {
   const NoteListView({super.key});
 
+
+  final data = const [
+    
+  ];
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (context, index) {
-        return const NoteItem();
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 4),
+          child: const NoteItem(),
+        );
       } ,
     );
   }
